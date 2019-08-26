11f17d42d4a3e023a18b56cd4d2b5c0a2fcb6fb1
juanmi <juanmixp@gmail.com>
2019-08-26 13:11:54 +0200

    added comments in IBM implementation

--- code/src/grovers-two-qubit.py
+++ code/src/grovers-two-qubit.py
@@ -3,38 +3,40 @@ from qiskit import QuantumCircuit
 from qiskit.circuit.quantumcircuit import QuantumCircuit
 from qiskit.visualization import plot_histogram
 
+
+
 backend = BasicAer.get_backend('qasm_simulator')
 #backend = BasicAer.get_backend('statevector_simulator')
+
+#Quantum register
+#https://quantumcomputing.stackexchange.com/questions/4907/qiskit-is-there-any-way-to-discard-the-results-of-a-measurement
 
 q = QuantumRegister(2, 'q')
 a = QuantumRegister(1, 'a')
 c = ClassicalRegister(2, 'c')
 
-
+#Circuit
 circ = QuantumCircuit(q,a,c) # type: qiskit.circuit.quantumcircuit.QuantumCircuit
 
-
+# ===== prepare the states: psi^{[0]} =====
 circ.iden(q[0])
 circ.iden(q[1])
 circ.x(a[0])
-# Add a H gate on qubit 0, putting this qubit in superposition.
+
+#===== Initialization: psi^{[1]} =====
 circ.h(q[0])
 circ.h(q[1])
 circ.h(a[0])
 circ.barrier(q)
-# Add a CX (CNOT) gate on control qubit 0 and target qubit 1, putting
-# the qubits in a Bell state.
-#circ.cx(q[0], q[1])
-# Add a CX (CNOT) gate on control qubit 0 and target qubit 2, putting
-# the qubits in a GHZ state.
-#circ.cx(q[0], q[2])
 
+#===== Sign flip: psi^{[2]} =====
 circ.x(q[0])
 circ.ccx(q[0], q[1], a[0])
 circ.x(q[0])
 
 circ.barrier(q)
 
+#===== Inversion about average: psi^{[3]} =====
 circ.h(q[0])
 circ.h(q[1])
 circ.x(q[0])
@@ -48,22 +50,23 @@ circ.h(q[0])
 circ.h(q[1])
 
 
+#===== Meaurement =====
 circ.measure(q,c)
 
-
-#
+#job
 job = execute(circ, backend=backend, shots=1600)
 result = job.result()
-#
-figura = circ.draw(output='mpl')
-figura.savefig('kk2.png')
 
+#circuit
+figure = circ.draw(output='mpl')
+figure.savefig('../data/images/qiskit-circuit.png')
+
+#histogram
 counts = result.get_counts(circ)
+print("Total counts:")
 print(counts)
 
-figura = plot_histogram(counts)
-figura.savefig('kk.png')
+figure = plot_histogram(counts)
+figure.savefig('../data/images/qiskit-histogram.png')
 
-#vector = result.get_statevector(circ)
-#print(vector)
 

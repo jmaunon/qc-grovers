07bb7f8e6e62897f525f1ef339a7f1ae2125df34
juanmixp <juanmixp@gmail.com>
2019-09-06 17:47:10 +0200

    added texlive-full

--- texlive/texlive_install.sh
+++ texlive/texlive_install.sh
@@ -10,6 +10,7 @@
 # See if there is a cached version of TL available
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
 if ! command -v texlua > /dev/null; then
+  sudo apt-get install texlive-full
   # Obtain TeX Live
   wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
   tar -xzf install-tl-unx.tar.gz
